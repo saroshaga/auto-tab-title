5facffaa65693eb98bddbfd0e0a93fe1cde0a4b6
saroshaga <sarosh@quodeit.com>
2019-01-06 18:12:44 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# auto-tab-title package
+
+Mirrors first line of an untitled file to its tab title on command.
---
+++ spec/auto-tab-title-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutoTabTitleView from '../lib/auto-tab-title-view';
+
+describe('AutoTabTitleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
